c5dc74364b73f44e695c0e36206613d7e7d4aaa2
Burak Aksu <burakaksu576@gmail.com>
2023-07-05 09:47:08 +0300

    Hive manager fonksiyonları düzenlendi

--- lib/hive/hive_manager.dart
+++ lib/hive/hive_manager.dart
@@ -1,10 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:hive_flutter/hive_flutter.dart';
+
 class HiveHelper {
-  HiveHelper._init();
+  static Future<void> createBox(String boxName) async {
+    await Hive.openBox(boxName);
+  }
 
-  static HiveHelper? _instance;
+  static bool checkBoxIsOpen(Box box) {
+    if (box.isOpen) return true;
+    return false;
+  }
 
-  static HiveHelper get instance {
-    _instance ??= HiveHelper._init();
-    return _instance!;
+  static Future<bool> closeBox(Box box) async {
+    await box.close();
+    if (box.isOpen) return false;
+    return true;
+  }
+
+  static Future<void> putItem(Box box, String key, dynamic value) async {
+    await box.put(key, value);
+  }
+
+  static Future<void> deleteItem(Box box, String key) async {
+    await box.delete(key);
   }
 }
